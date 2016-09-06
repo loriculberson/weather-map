4da11dfa511b95f34e237b7159ab78660fd964cd
Lori Culberson <bigskybandit@sbcglobal.net>
2016-09-05 23:02:09 -0500

    First commit and Add map background

---
+++ index.ios.js
@@ -0,0 +1,22 @@
+var React = require('react-native');
+var {
+  AppRegistry,
+  MapView,
+  View,
+  StyleSheet
+} = React;
+
+var Weather = React.createClass({
+  render: function() {
+    return <MapView style={styles.map}></MapView>
+
+  }
+});
+
+var styles = StyleSheet.create({
+  map: {
+    flex: 1
+  }
+});
+
+AppRegistry.registerComponent('weather', () => Weather);
